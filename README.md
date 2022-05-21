6885e51fe5d4385792c885c7b4d923f55c651387
thugiang <xanhtham.cuc@gmail.com>
2022-05-21 15:50:23 +1200

    use event propagation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-### This is a practice for DOMmanupilations using jQuery
+### A coding practice for DOMmanupilations using jQuery
+## This is a code-along lesson that is a part of Altcademy course
--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 //III. Use each() to print each value on screen
 //IV. Calculate total portfolio value and profit
 //V.Add Javascript to remove button
-
-
+//VI. Make Shae, Cost, & Market Price editable
+//VII. Attach event handler to value change on inputs
+//VIII. Add new row to the list
 
 // get shars owned and market price
 //wrap it inside ready func: script file is loaded before the HTML body element
@@ -12,8 +13,10 @@ const updateMarketValue = function (element) {
   //get shares Owned & market price by looping through tr
   //tr.children(.targetClass).text-> retreive text of that child element
   //parsefloart: return string into a floating number
-  const sharesOwned = parseFloat($(element).children(".shares").text());
-  const marketPrice = parseFloat($(element).children(".marketPrice").text());
+  //val: retrieve value from input
+  //find(): look for nested children elements
+  const sharesOwned = parseFloat($(element).find(".shares input").val());
+  const marketPrice = parseFloat($(element).find(".marketPrice input").val());
 
   //market value = shares Owned * market price per share
   const marketValue = sharesOwned * marketPrice;
@@ -25,9 +28,9 @@ const updateMarketValue = function (element) {
   return marketValue;
 };
 
-var updateUnrealizedProfit = function (element, marketValue) {
-  const sharesOwned = parseFloat($(element).children(".shares").text());
-  const costPerShare = parseFloat($(element).children(".cost").text());
+const updateUnrealizedProfit = function (element, marketValue) {
+  const sharesOwned = parseFloat($(element).find(".shares input").val());
+  const costPerShare = parseFloat($(element).find(".cost input").val());
   const costOfPurchase = sharesOwned * costPerShare;
 
   //unrealised profit = marketValue - costOfPurchase
@@ -41,8 +44,7 @@ const sum = function (account, x) {
   return account + x;
 };
 
-  var updatePortfolioValueAndProfit = function() {
-
+var updatePortfolioValueAndProfit = function () {
   //updateMarkektValue() & updateUnrealisedProfit() already return individuals
   //store themm in arrays and sum them up
   const stocksMarketValue = [];
@@ -58,19 +60,86 @@ const sum = function (account, x) {
     const unrealizedProfit = updateUnrealizedProfit(element, marketValue);
     stocksUnrealizedProfits.push(unrealizedProfit);
   });
-      
-    //The reduce() method returns a single value: the function's accumulated result.
-    const portfolioMarketValue = stocksMarketValue.reduce(sum);
-    const portfolioUnrealizedProfit = stocksUnrealizedProfits.reduce(sum);
-    
-    
-    //inject the value in span
-      $("#portfolioValue").html(portfolioMarketValue);
-      $("#portfolioProfit").html(portfolioUnrealizedProfit);
 
-}
+  //The reduce() method returns a single value: the function's accumulated result.
+  const portfolioMarketValue = stocksMarketValue.reduce(sum);
+  const portfolioUnrealizedProfit = stocksUnrealizedProfits.reduce(sum);
 
-//use ready func after the HTML is ready
-$(document).ready(function () { 
+  //inject the value in span
+  $("#portfolioValue").html(portfolioMarketValue);
+  $("#portfolioProfit").html(portfolioUnrealizedProfit);
+};
+
+//ufire as soon as DOM becomes safe to manipulate
+$(document).ready(function () {
+  updatePortfolioValueAndProfit();
+
+  $(document).on("click", ".btn.remove", function (event) {
+    //closest: finds the closest parent element that matches the CSS selector 'tr'
+    //remove(): remove selected element from DOM
+    $(this).closest("tr").remove();
+    // = $(this).parent().parent().remove();
     updatePortfolioValueAndProfit();
-}
+  });
+
+  //Use event listener change input to trigger an update
+  //.on(): fired synchronously as the value of an input element changes.
+    $(document).on("input", "tr input", function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updatePortfolioValueAndProfit();
+    }, 1000);
+  });
+
+  //get all of the new values
+  $("#addStock").on("submit", function (event) {
+    event.preventDefault();
+    var name = $(this).children("[name=name]").val();
+
+    var shares = $(this).children("[name=shares]").val();
+
+    var cost = $(this).children("[name=cost]").val();
+
+    var marketPrice = $(this).children("[name=marketPrice]").val();
+
+    $("tbody").append(
+      "<tr>" +
+        '<td class="name">' +
+        name +
+        "</td>" +
+        '<td class="shares"><input type="number" value="' +
+        shares +
+        '" /></td>' +
+        '<td class="cost"><input type="number" value="' +
+        cost +
+        '" /></td>' +
+        '<td class="marketPrice"><input type="number" value="' +
+        marketPrice +
+        '" /></td>' +
+        '<td class="marketValue"></td>' +
+        '<td class="profit"></td>' +
+        '<td><button class="btn btn-dark btn-sm remove">remove</button></td>' +
+        "</tr>"
+    );
+
+    updatePortfolioValueAndProfit();
+    $(this).children("[name=name]").val("");
+    $(this).children("[name=shares]").val("");
+    $(this).children("[name=cost]").val("");
+    $(this).children("[name=marketPrice]").val("");
+  });
+});
+
+//updatePortfolioValueAndProfit is a big function: loop and manupilate DOM
+//when portfolio getslonger, update function will start to be sluggish -> use a debounce
+//debounce: make sure time-consuming tasks do not fire so often -> improve performance of the web page
+
+//call the update when a certain period has passed after the user stopped typing
+var timeout;
+$("tr input").on("input", function () {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    updatePortfolioValueAndProfit();
+  }, 1000); // delay 1s ->update wouldn't be executed until 1 second after the user has stopped typing
+  //when new change is made -> timeout for another 1 sec& old one is destroyed
+});
